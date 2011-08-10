66ebedb3a70c9d1c75a515cff73c7986b9da1b2a
William R. Fraser <wfraser@smokey.codewise.org>
2011-08-10 14:42:16 -0700

    add some spacing to flacmp3.sh

--- flacmp3.sh
+++ flacmp3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $ARGC != 2 ]]; then
+if [[ $# != 2 ]]; then
     echo -e "usage: $0 <srcdir> <dstdir>"
     echo -e "\tconverts all .flac files in <srcdir> to .mp3 in <dstdir>, renaming file.flac as __file.mp3"
     exit
@@ -12,5 +12,14 @@ dstdir=$2
 echo "srcdir($srcdir)"
 echo "dstdir($dstdir)"
 
-find "$srcdir" -name '*.flac' -print0 | xargs -0 -L1 -P0 bash -c 'srcdir=$0; dstdir=$1; old=$2; new=$(echo $old | sed -r "s/\.flac$/.mp3/; s/^.*\/([^/]+)$/__\1/"); echo -e "converting $old\n\t$dstdir/$new"; sox "$old" "$dstdir/$new"; echo "$dstdir/$new done"' "$srcdir" "$dstdir"
+find "$srcdir" -name '*.flac' -print0 \
+    | xargs -0 -L1 -P0 bash -c '
+        srcdir=$0;
+        dstdir=$1;
+        old=$2; 
+        new=$(echo $old | sed -r "s/\.flac$/.mp3/; s/^.*\/([^/]+)$/__\1/");
+        echo -e "converting $old\n\t$dstdir/$new";
+        sox "$old" "$dstdir/$new"; 
+        echo "$dstdir/$new done"' \
+            "$srcdir" "$dstdir"
 
